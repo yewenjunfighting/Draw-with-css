cffbc0386157de785c3a2e8a1d2ae27b9e32ca50
ywj <1728914873@qq.com>
2018-10-14 18:48:15 +0800

    Opera Logo

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <OperaLogo/>
+  </div>
+</template>
+
+<script>
+import OperaLogo from './components/OperaLogo.vue'
+
+export default {
+  name: 'app',
+  components: {
+    OperaLogo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/OperaLogo.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="opera">
+    <div class="opera-top"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OperaLogo'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .opera{
+    width: 258px;
+    height: 275px;
+    background: #F22629;
+    border-radius: 258px 258px 258px 258px /275px 275px 275px 275px;
+    position: relative;
+  }
+
+  .opera-top{
+    width: 112px;
+    height: 231px;
+    background: #fff;
+    border-radius: 112px 112px 112px 112px /231px 231px 231px 231px;
+    position: absolute;
+    left: 50%;
+    right: 50%;
+    top: 50%;
+    bottom: 50px;
+    margin-left: -56px;
+    margin-top: -115px;
+  }
+</style>
